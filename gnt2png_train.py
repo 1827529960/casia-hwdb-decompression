0ede9402ae3a92eacdfbcaf12b635ec817c6c2cd
有点无语h <1827529960@qq.com>
2020-10-30 21:16:47 +0800

    my first commit

---
+++ gnt2png_train.py
@@ -0,0 +1,42 @@
+#coding=utf-8
+import struct
+import os
+from PIL import Image
+DATA_PATH="./HWDB1.1trn_gnt/" #gnt数据文件路径
+IMG_PATH="./data/train"#解析后的图片存放路径
+files=os.listdir(DATA_PATH)
+num=0
+for file in files:
+    tag = []
+    img_bytes = []
+    img_wid = []
+    img_hei = []
+    f=open(DATA_PATH+"/"+file,"rb")
+    while f.read(4):
+        tag_code=f.read(2)
+        tag.append(tag_code)
+        width=struct.unpack('<h', bytes(f.read(2)))
+        height=struct.unpack('<h',bytes(f.read(2)))
+        img_hei.append(height[0])
+        img_wid.append(width[0])
+        data=f.read(width[0]*height[0])
+        img_bytes.append(data)
+    f.close()
+    for k in range(0, len(tag)):
+        im = Image.frombytes('L', (img_wid[k], img_hei[k]), img_bytes[k])
+        if os.path.exists(IMG_PATH + "/" + tag[k].decode('gbk')):
+            im.save(IMG_PATH + "/" + tag[k].decode('gbk') + "/" + str(num) + ".png")
+        else:
+            os.mkdir(IMG_PATH + "/" + tag[k].decode('gbk'))
+            im.save(IMG_PATH + "/" + tag[k].decode('gbk') + "/" + str(num) + ".png")
+    num = num + 1
+print(tag.__len__())
+
+files=os.listdir(IMG_PATH)
+n=0
+f=open("label.txt","w") #创建用于训练的标签文件
+for file in files:
+    files_d=os.listdir(IMG_PATH+"/"+file)
+    for file1 in files_d:
+        f.write(file+"/"+file1+" "+str(n)+"\n")
+    n=n+1
